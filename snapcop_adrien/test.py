d2767e98cc9812d56f82a73a5d9fdf3b58fe1013
Pc_Banc_Photo <declic@declic-pc.(none)>
2012-08-13 16:58:45 +0200

    prise de vue 2

--- snapcop_adrien/test.py
+++ snapcop_adrien/test.py
@@ -6,19 +6,5 @@ from pygame.locals import *
 pygame.display.init()
 pygame.joystick.init()
 
-try:
-    stick = pygame.joystick.Joystick(0)
-    stick.init()
-    print 'joystick trouvé !'
-except:
-    print 'pas de joystick !!'
-    sys.exit()
-    
-while True:
-    evts = pygame.event.get()
-    if len(evts) > 0:
-        for evt in evts:
-            if evt.type == pygame.locals.JOYBUTTONDOWN:
-                i = evt.button
-                frames = ((i-3)*5)
-                print frames
+cmd = 'mkdir output; for i in *.jpg; do convert -resize 800x600 -quality 50 "$i" "output/$i"; done'
+subprocess.check_output(cmd, shell=True)
